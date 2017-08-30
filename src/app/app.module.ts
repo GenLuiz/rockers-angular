a5335aacf6bd48dbcd2579949d70ea6a7d08aee0
Barbosa, Luiz <luiz.barbosa@wipro.com>
2017-08-30 10:13:30 -0300

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { ContractComponent } from './contract/contract.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    HomeComponent,
+    TeamComponent,
+    ContractComponent
   ],
   imports: [
     BrowserModule
